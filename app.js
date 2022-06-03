82c532696a110e708430b73e8e6372253c1ccb9e
Kevin-beni <benibiantuadi@gmail.com>
2022-06-03 03:43:03 +0200

    creation de post a sauce, get a sauce, get array of sauces

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const exppress = require("express");
 const app = exppress();
 const cors = require("cors");
+const Thing = require("./models/Thing");
 
 // middleware
 app.use(cors());
@@ -8,6 +9,7 @@ app.use(exppress.json());
 
 // connection à la base de données mongoDB
 const mongooose = require("mongoose");
+const { del } = require("express/lib/application");
 const mongoURI = "mongodb+srv://kevin:CE4MFtZ4kiF1JTH5@cluster0.y0gnr7k.mongodb.net/?retryWrites=true&w=majority";
 mongooose.connect( mongoURI, { useNewUrlParser: true, useUnifiedTopology: true } )
     .then(() => console.log("Connexion à la base de données réussie !"))
@@ -16,13 +18,34 @@ mongooose.connect( mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }
 
 
 // routes
-app.post("/api/auth/signup", (req, res) => {
+
+// post login and signup
+app.post("/api/auth/signup", (req, res) => {// signup
 })
-app.post("/api/auth/login", (req, res) => {
+app.post("/api/auth/login", (req, res) => {// login
 })
 
+// post and get sauces
+app.post("/api/sauces", (req, res) => {// post a sauce
+    delete req.body._id;
+    const thing = new Thing({
+        ...req.body,
+    });
+    thing.save()
+        .then(() => res.send(thing))
+        .catch(err => res.status(400).send(err));
+})
+app.get("/api/sauces:id", (req, res) => {// get a sauce
+    Thing.findOne({_id: req.params.id})
+        .then(thing => res.send(thing))
+        .catch(err => res.status(404).send(err));
+})
 
-app.get("/api", (req, res) => {
+// get all sauces
+app.get("/api", (req, res) => { // get array of sauces
+    Thing.find()
+        .then(things => res.send(things))
+        .catch(err => res.status(400).send(err));
 });
 
 // exportation du module
