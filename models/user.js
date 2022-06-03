250cede0edb88ca923728168feee1ffb01399ab0
Kevin-beni <benibiantuadi@gmail.com>
2022-06-03 04:32:11 +0200

    configuration d'empecher plusieur users à créer des comptes avec plusieurs adress mails

--- models/user.js
+++ models/user.js
@@ -1,8 +1,11 @@
 const mongooose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const uerShema = new mongooose.Schema({
   email: { type: String, required: true , unique: true},
   password: { type: String, required: true },
 });
+
+uerShema.plugin(uniqueValidator);
 
 module.exports = mongooose.model("User", uerShema);
