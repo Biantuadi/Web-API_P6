9ec87248b8b1cdf6747afbd82a26310ad28a752f
Kevin-beni <benibiantuadi@gmail.com>
2022-06-06 04:30:17 +0200

    debut de la partie authentifiaction

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -12,27 +12,29 @@ exports.signup = (req, res) => {
       user
         .save()
         .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
-        .catch((error) => res.status(409).json({ massage : "Utilisateur pas enregistrer" + error }));
+        .catch((error) =>
+          res
+            .status(409)
+            .json({ massage: "Utilisateur pas enregistrer" + error })
+        );
     })
     .catch((error) => res.status(500).json({ error }));
 };
 
-// exports.login = (req, res) => {
-//   User.findOne({ email: req.body.email })
-//     .then((user) => {
-//       if (!user)
-//         return res.status(401).json({ error: "Utilisateur non trouvé !" });
-//       bcrypt
-//         .compare(req.body.password, user.password)
-//         .then((valid) => {
-//           if (!valid)
-//             return res.status(401).json({ error: "Mot de passe incorrect !" });
-//           res.status(200).json({
-//             userId: user._id,
-//             token: "Bearer token",
-//           });
-//         })
-//         .catch((error) => res.status(500).json({ error }));
-//     })
-//     .catch((error) => res.status(500).json({ error }));
-// };
+exports.login = (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user)
+        return res.status(401).json({ error: "Utilisateur non trouvé !" });
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid)
+            return res.status(401).json({ error: "Mot de passe incorrect !" });
+          else
+            res.status(200).json({ userId: user._id, token: "Bearer token" });
+        })
+        .catch((error) => res.status(500).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
